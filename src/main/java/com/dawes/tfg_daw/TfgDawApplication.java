ef8d1ce4ec4fbae50e02612f65d4bf6fcd64ada9
yolap <yolapsn@gmail.com>
2024-01-23 22:11:25 +0100

    Initial commit

---
+++ src/main/java/com/dawes/tfg_daw/TfgDawApplication.java
@@ -0,0 +1,13 @@
+package com.dawes.tfg_daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgDawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawes/tfg_daw/TfgDawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawes.tfg_daw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgDawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
